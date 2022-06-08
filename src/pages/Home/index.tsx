411f4dd2260e3a19d81e491516d3fe4b6a713181
Thiago <thiagocalil@live.com>
2022-06-08 15:23:12 -0300

    Add cartItemsAmount with dummy values

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,16 +24,15 @@ interface CartItemsAmount {
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
 
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount;
+    return sumAmount;
+  }, {} as CartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
-
       const response = await api.get('/products');
 
       setProducts(
@@ -48,10 +47,9 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
-  console.log(products);
   return (
     <ProductList>
       {products.map((product) => (
@@ -62,11 +60,11 @@ const Home = (): JSX.Element => {
           <button
             type='button'
             data-testid='add-product-button'
-            // onClick={() => handleAddProduct(product.id)}
+            onClick={() => handleAddProduct(product.id)}
           >
             <div data-testid='cart-product-quantity'>
               <MdAddShoppingCart size={16} color='#FFF' />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
